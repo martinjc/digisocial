16eabfb36e896ed90c3505095035735baf73d74e
Will Webberley <willwebberley@gmail.com>
2012-09-15 17:11:10 +0100

    Update for outcomes to views.py

--- src/digisoc/api/views.py
+++ src/digisoc/api/views.py
@@ -7,7 +7,6 @@ import outcome_methods
 import food_methods
 
 def crimes(request):
-
     #
     # Prelim
     if request.method != 'GET':
@@ -33,15 +32,42 @@ def crimes(request):
     try:
         crimeList = crime_methods.retrieveCrimes(ne, sw, startYear, startMonth, endYear, endMonth)
         crime_data = {'crimes': crimeList}
+    	return return_data(request, crime_data)
+    except Exception as e:
+        return HttpResponseBadRequest( "Something went wrong. Blame Greenwood: %s" % e )
+
+def outcomes(request):
+    #
+    # Prelim
+    if request.method != 'GET':
+        return HttpResponseBadRequest("GET calls only")
+
+    params = request.GET
+
+    req_params = ["ne", "sw"]
+    for req_param in req_params:
+        if req_param not in params:
+            return HttpResponseBadRequest("missing param: %s" %(req_param))
+
+    #
+    # Grab params
+    ne = params['ne']
+    sw = params['sw']
+
+    startYear = params.get('sy', None)
+    startMonth = params.get('sm', None)
+    endYear = params.get('ey', None)
+    endMonth = params.get('em', None)
+
+    try:
         outcomeList = outcome_methods.retrieveOutcomes(ne, sw, startYear, startMonth, endYear, endMonth)
         outcome_data = {'outcomes': outcomeList}
-    	return return_data(request, crime_data)
+    	return return_data(request, outcome_data)
     except Exception as e:
         return HttpResponseBadRequest( "Something went wrong. Blame Greenwood: %s" % e )
 
 
 def food_ratings(request):
-
     #
     # Prelim
     if request.method != 'GET':
