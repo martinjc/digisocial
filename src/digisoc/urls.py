360d9626d25f510ad547fe549e284fd0535a4579
Martin Chorley <martin.chorley@gmail.com>
2012-09-15 12:06:58 +0100

    basic setup

--- src/digisoc/urls.py
+++ src/digisoc/urls.py
@@ -5,13 +5,6 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'digisoc.views.home', name='home'),
-    # url(r'^digisoc/', include('digisoc.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include( 'api.urls' ) ),
+    url(r'^$', 'digisoc.frontend.views.index'),
 )
