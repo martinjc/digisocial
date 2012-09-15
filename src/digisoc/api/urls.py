052b8a00d33f446f6db469f121c35317078c5afc
Martin Chorley <martin.chorley@gmail.com>
2012-09-15 12:25:47 +0100

    basic api call (for testing)

---
+++ src/digisoc/api/urls.py
@@ -0,0 +1,48 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('',
+
+    """
+    Request crime data for area, given by north east and south west coordinates.
+
+    GET parameters:
+
+    ne - north east coordinate of bounding box
+    sw - south west coordinate of bounding box
+
+    returns: json list of crimes within bounding box:
+
+    {
+        crimes: [
+            crime: {
+                point: {
+                    lat: X1,
+                    lng: Y1
+                }
+                value: Z1
+            },
+            crime: {
+                point: {
+                    lat: X2,
+                    lng: Y2
+                }
+                value: Z2
+            },
+            crime: {
+                point: {
+                    lat: X3,
+                    lng: Y3
+                }
+                value: Z3
+            }
+        ]
+    }
+
+
+    usage:
+
+    /crimes/?ne=X.XXXXXXX&sw=Y.YYYYYYY
+
+    """
+    url(r'crimes/?$', 'digisoc.api.views.crimes'),
+)
--- src/digisoc/api/views.py
+++ src/digisoc/api/views.py
@@ -1,1 +1,48 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest
+
+import json
+
+def crimes(request):
+
+    #
+    # Prelim
+    if request.method != 'GET':
+        return HttpResponseBadRequest("GET calls only")
+
+    params = request.GET
+
+    req_params = ["ne", "sw"]
+    for req_param in req_params:
+        if req_param not in params:
+            return HttpResponseBadRequest("missing param: %s" %(req_param))
+
+    #
+    # Grab params
+    ne_p = params['ne']
+    sw_p = params['sw']
+    try:
+        ne = float(ne_p.strip())
+        sw = float(sw_p.strip())
+    except Exception:
+        return HttpResponseBadRequest("failed to parse params: %s, %s" % (ne_p, sw_p))
+
+    #
+    # Here's the magic, needs completing:
+    #try:
+    #    crime_data = ""
+    #    return HttpResponse( crime_data )
+    #except Exception:
+    #    return HttpResponseBadRequest( "Something went wrong. Blame Greenwood." )
+
+    crime_data = {'crimes': [{'point': {'lat': 51.482, 'lng': -3.186}, 'value': 4}, {'point': {'lat': 51.482, 'lng': -3.186}, 'value': 4}, {'point': {'lat': 51.482, 'lng': -3.186}, 'value': 4}]}
+    return_data(request, crime_data)
+
+def return_data(request, data_dict):
+
+    callback = request.GET.get('callback', None)
+
+    if callback is None:
+        return HttpResponse(json.dumps(data_dict), mimetype = 'application/json')
+    else:
+        return HttpResponse(callback + '(' + json.dumps(data_dict) + ')', mimetype = 'application/javascript')
